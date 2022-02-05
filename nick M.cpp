8f795f02f7221bb0b4bf36706e91a6ef600cf982
Mirabito15 <99060463+Mirabito15@users.noreply.github.com>
2022-02-04 23:32:15 -0500

    Add files via upload

---
+++ nick M.cpp
@@ -0,0 +1,9 @@
+Variable N1 is the random number generated between 1 to 100
+Get user input from N2 variable.
+In case N2! = N1 is true:
+.if N2>N1 true, write H;
+. else, write L.
+			Following this, write W, which is used for an incorrect guess.
+Get user input from variable N2 again
+In case N2! = N1 is flase, write R.
+
